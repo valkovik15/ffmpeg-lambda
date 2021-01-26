cf28888eac1b11177bd4bf9273e70756dd57feeb
Valentin Kopotchel <valentin.kopothcel@cybergizer.com>
2021-01-26 13:12:02 +0300

    Adapt for storing both mp4 and webm

--- handler.rb
+++ handler.rb
@@ -3,7 +3,7 @@ require 'digest'
 require 'aws-sdk-s3'
 require 'net/http'
 require 'net/https'
-
+require 'securerandom'
 
 def convert(event:, context:)
     record = event['Records'].first
@@ -16,15 +16,17 @@ def convert(event:, context:)
     return if client.head_object(bucket: bucket_name, key: file_name).to_h[:content_type]=='video/mp4'
     f = File.new(local_path, 'w+')
     return unless client.get_object(response_target: local_path, bucket: bucket_name, key: file_name)
-    `/opt/ffmpeg/ffmpeg -i #{local_path} -movflags +faststart -c:v libx264 -pix_fmt yuv420p -profile:v main -level 3.0 -b:v 2.5M -c:a aac -b:a 128k -r 30 #{local_path}.mp4`
-    obj = s3.bucket(bucket_name).object(file_name)
-    client.put_object(bucket: bucket_name, key: file_name, body: File.open("#{local_path}.mp4"), content_type: 'video/mp4')
-    File.open("#{local_path}.mp4") { |file|
+    mp4_key = "#{SecureRandom.hex}#{Time.now.to_i}"
+    mp4_filename = "/tmp/#{mp4_key}"
+    `/opt/ffmpeg/ffmpeg -i #{local_path} -movflags +faststart -c:v libx264 -pix_fmt yuv420p -profile:v main -level 3.0 -b:v 2.5M -c:a aac -b:a 128k -r 30 #{mp4_filename}.mp4`
+    client.put_object(bucket: bucket_name, key: mp4_key, body: File.open("#{mp4_filename}.mp4"), content_type: 'video/mp4')
+    File.open("#{mp4_filename}.mp4") { |file|
         logs = { 
             content_md5: Digest::MD5.file(file).base64digest,
             byte_size: File.size(file).to_s,
-            key: file_name,
-            content_type: 'video/mp4'
+            key: mp4_key,
+            content_type: 'video/mp4',
+            webm_key: file_name
             }
         uri = URI.parse(ENV['API_ENDPOINT'])
         http = Net::HTTP.new(uri.host, uri.port)
@@ -35,7 +37,7 @@ def convert(event:, context:)
         resp = http.request(request)
     }
     File.delete(f.path) if File.exist?(f.path)
-    File.delete("#{f.path}.mp4") if File.exist?("#{f.path}.mp4")
+    File.delete("#{mp4_filename}.mp4") if File.exist?("#{mp4_filename}.mp4")
   {
     statusCode: 200,
     body: {
